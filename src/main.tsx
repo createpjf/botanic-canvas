e749bfd869f541e38ff71c13fa650cf14d4f6ebe
createpjf <113523690+createpjf@users.noreply.github.com>
2026-08-29 23:26:30 +0800

    接入 Vercel Speed Insights，跟未合并的 #91 同一套埋点。

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App'
 import { ProductI18nProvider } from './i18n/react'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ProductI18nProvider><App /></ProductI18nProvider>
+    <ProductI18nProvider>
+      <App />
+      <SpeedInsights />
+    </ProductI18nProvider>
   </StrictMode>,
 )
